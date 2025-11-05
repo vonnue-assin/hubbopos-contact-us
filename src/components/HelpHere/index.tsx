22de8b352113bd36de3c00c43d2c7796adc26f1a
vonnue-assin <assin.anna@vonnue.com>
2025-11-05 10:24:40 +0530

    feat: implemented new contact page

---
+++ src/components/HelpHere/index.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+const HelpHere = () => {
+  return (
+    <div className="help-here-container-main">
+      <h1 className="help-content-header">We're Here to Help</h1>
+      <p className="help-content-paragraph">
+        Couldn't find what you were looking for? No worries, we're here to help!
+      </p>
+    </div>
+  );
+};
+
+export default HelpHere;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import Header from "../../components/Header";
+import HelpHere from "../../components/HelpHere";
 
 export const Home = () => {
   return (
     <div>
       <Header />
+      <HelpHere />
     </div>
   );
 };
